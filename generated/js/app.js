eb0158791d272374eb3977c2b6f4edfeaca56b74
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-03-23 11:20:24 +0100

    Video API working for every languages

--- generated/js/app.js
+++ generated/js/app.js
@@ -57872,7 +57872,7 @@ angular.module('app')
             getOne: function(query) {
                 var reqvideo = {
                     method: 'GET',
-                    url: "https://api.cognitive.microsoft.com/bing/v5.0/videos/search?q=" + query + "&count=10&offset=0&mkt=en-us&safeSearch=Moderate",
+                    url: "https://api.cognitive.microsoft.com/bing/v5.0/videos/search?q=" + query + "&count=10&offset=0&safeSearch=Moderate",
                     headers: {
                         'Ocp-Apim-Subscription-Key': 'cf968acca48c492b88c535945b332bf0'
                     }
