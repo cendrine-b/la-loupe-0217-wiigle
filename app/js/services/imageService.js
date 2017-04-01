2dbc5801f3f158a47a2815313764d8f830a21431
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-04-01 18:23:44 +0200

    Functional back-to-top button

--- app/js/services/imageService.js
+++ app/js/services/imageService.js
@@ -9,7 +9,7 @@ angular.module('app')
             method: 'GET',
             url: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + query + "&count=10&offset=0&mkt=en-us&safeSearch=Moderate",
             headers: {
-              'Ocp-Apim-Subscription-Key' : 'c6482be0e97640cfaf43b135b9615b55'
+              'Ocp-Apim-Subscription-Key' : '9a7fe6e652c0485dbbf8946a0b2a8ade'
             }
           };
             return $http(reqimage);
