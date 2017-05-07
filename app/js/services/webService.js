d6bc4d4fca40dd9fb97a925022d2cdbc7b2d9472
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-05-07 15:37:00 +0200

    New Bing API key

--- app/js/services/webService.js
+++ app/js/services/webService.js
@@ -6,7 +6,7 @@ angular.module('app')
             method: 'GET',
             url: "https://api.cognitive.microsoft.com/bing/v5.0/search?q=" + query + "&count=1",
             headers: {
-              'Ocp-Apim-Subscription-Key' : '6deba95f5e1f423a81e05087bd45fc54'
+              'Ocp-Apim-Subscription-Key' : '422b0d919297451eb41dc323a80bac58'
             }
           };
             return $http(reqweb);
